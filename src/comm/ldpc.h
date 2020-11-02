37431ed97e1e60892416f9781d35a0a4a117e99c
Vijaya Kumar <i.am.viji@gmail.com>
2020-11-03 00:33:01 +0530

    resolving compilation error

--- src/comm/ldpc.h
+++ src/comm/ldpc.h
@@ -26,11 +26,11 @@ namespace py = pybind11;
 void generate_pybind_wrapper_for_itpp_ldpc_code_class ( py::module &m) {
 
   py::class_<itpp::LDPC_Parity>( m, "ldpc_parity")
-    .def( py::init<())
+    .def( py::init<>())
   ;
 
   py::class_<itpp::LDPC_Parity_Unstructured, itpp::LDPC_Parity>( m, "ldpc_parity_unstructured" )
-    .def( py::init<())
+    .def( py::init<>())
   ;
 
   py::class_<itpp::LDPC_Parity_Regular, itpp::LDPC_Parity_Unstructured>( m, "ldpc_parity_regular" )
